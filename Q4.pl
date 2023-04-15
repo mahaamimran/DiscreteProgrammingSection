a113782914d6962d18870c045110ea054f4b8c8f
mahaamimran <mahaamimran@gmail.com>
2023-04-15 12:29:32 +0500

    nonplagerisedq4

--- Q4.pl
+++ Q4.pl
@@ -1,51 +1,49 @@
-% Define dynamic predicates for tasks and their completion status
+% By declaring task/2 as dynamic the program can modify the completion status of tasks at runtime
+% the 2 represents that it takes 2 arguments, the task name and the completion status
 :- dynamic task/2.
 
-% Initialize tasks
-task(work, false).
-task(study, false).
-task(exercise, false).
+% Initializing
+task(work,false).
+task(study,false).
+task(exercise,false).
+task(sleep,false).
+task(eat,false).
 
-% Rule to mark a task as completed
-complete_task(Task) :-
-    retract(task(Task, _)),
-    assert(task(Task, true)).
+% Rule 1
+complete_task(X):-
+    % removing the old task first
+    retract(task(X,_)),
+    % adding the new task
+    assert(task(X,true)).
 
-% Rule to check if a task is completed
-task_completed(Task) :-
-    task(Task, true).
+% Rule 2
+task_completed(Y):-
+    task(Y,true).
 
-% Rule to check if a task is not completed
-task_not_completed(Task) :-
-    task(Task, false).
-
-% Rule to display tasks and their completion status
-display_tasks :-
-    write('Task\t\tCompletion Status'), nl,
+% Rule 3
+display:-
+    write('Task\t\tCompled/Not Completed'), nl,
     write('-----------------------------------'), nl,
     task(Task, Completion),
     write(Task), write('\t\t'),
-    (Completion = true -> write('Completed') ; write('Not Completed')),
-    nl,
+    (Completion = true -> write('Completed') ; write('Not Completed')),nl,
     fail.
-display_tasks.
+display.
 
-% Rule to add a new task
-add_task(Task) :-
-    \+ task(Task, _), % Check if task doesn't already exist
-    assert(task(Task, false)),
-    write('Task added: '), write(Task), nl.
+% Rule 4
+add_task(Z) :-
+    \+ task(Z,_), % Check if task doesn't already exist
+    assert(task(Z, false)),
+    write('Task added: '), write(Z), nl.
 
 % Entry point for the program
 start :-
-    write('Welcome to Task Tracker!'), nl,
-    write('Available commands:'), nl,
-    write('add_task(Task). - Add a new task'), nl,
-    write('complete_task(Task). - Mark a task as completed'), nl,
-    write('task_completed(Task). - Check if a task is completed'), nl,
-    write('task_not_completed(Task). - Check if a task is not completed'), nl,
-    write('display_tasks. - Display all tasks and their completion status'), nl,
-    write('Type "halt." to exit.'), nl.
-
+    write('Welcome!'), nl,
+    write('What action would you like to perform?'), nl,
+    write('Adding a new task:\t\tadd_task(X).'), nl,
+    write('Mark task as completed:\t\tcomplete_task(Y).'), nl,
+    write('Check status of task:\t\ttask_completed(Z).'), nl,
+    write('Display tasks & status:\t\tdisplay.'), nl,
+    write('Enter "halt." to exit.'), nl.
 % Start the program
 :- start.
